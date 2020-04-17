6bcbaa4508a85689d53edee7b7cc08ad4ba99948
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-17 23:23:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice()
+def meal_choice(veg1, veg2, protein="tofu")
   puts "what a nutritious meal!"
+  meal = ""
+  
 end
